4722607c790fd8b19f720b81854971b88f54c8dd
Grusha5480 <MrGraphitem@gmail.com>
2018-06-25 01:34:12 +0300

    files.py changes

---
+++ files.py
@@ -0,0 +1,33 @@
+import json
+
+
+def create_site_list():
+    file = open('siteList.json', 'w')
+    file.write('{\n}')
+    file.close()
+
+
+def get_site_list():
+    file = open('siteList.json', 'r')
+    return json.loads(file)
+
+
+def save_site_list_obj(json_obj):
+    file = open('sileList.json', 'w')
+    file.write(json.dumps(json_obj))
+
+
+def create_user_data_file():
+    file = open('userData.json', 'w')
+    file.write('{\n}')
+    file.close()
+
+
+def get_user_data():
+    file = open('userData.json', 'r')
+    return json.loads(file)
+
+
+def save_user_data(json_obj):
+    file = open('userData.json', 'w')
+    file.write(json.dumps(json_obj))
