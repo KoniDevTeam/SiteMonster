553cec27dbe241ef2acea642d49160c128d6246e
Nikita Serba <nikitaserba@icloud.com>
2018-06-25 02:26:22 +0300

    Remove functions from files.py 'cause I don't know what the fuck they are doing there

--- files.py
+++ files.py
@@ -1,35 +1,7 @@
-import json
 import platform
 import os
 
 PATH = 'Koni Dev Team/Site Monster/'
-SITES_LIST_FILE = 'siteList.json'
-USER_DATA_FILES = 'userData.json'
-
-BLANK_SITE_LIST = {"sites": []}
-
-
-def get_site_list():
-    if os.path.exists(get_data_folder() + SITES_LIST_FILE):
-        file = open(get_data_folder() + SITES_LIST_FILE, 'r')
-        return json.loads(file)
-    else:
-        return BLANK_SITE_LIST
-
-
-def save_site_list_obj(json_obj):
-    file = open(get_data_folder() + SITES_LIST_FILE, 'w')
-    file.write(json.dumps(json_obj))
-
-
-def get_user_data():
-    file = open(get_data_folder() + USER_DATA_FILES, 'r')
-    return json.loads(file)
-
-
-def save_user_data(json_obj):
-    file = open(get_data_folder() + USER_DATA_FILES, 'w')
-    file.write(json.dumps(json_obj))
 
 
 def get_data_folder():
