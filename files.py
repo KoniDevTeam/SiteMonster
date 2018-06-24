8805ef6ecbeee1d36e6dd1f83310be9af995c3c1
Nikita Serba <nikitaserba@icloud.com>
2018-06-25 01:54:46 +0300

    Add function to get data folder

---
+++ files.py
@@ -0,0 +1,53 @@
+import json
+import platform
+import os
+
+PATH = 'Koni Dev Team/Site Monster/'
+SITES_LIST_FILE = 'siteList.json'
+
+BLANK_SITE_LIST = {"sites": []}
+
+def get_site_list():
+    if os.path.exists(get_data_folder() + SITES_LIST_FILE):
+        file = open(get_data_folder() + SITES_LIST_FILE, 'r')
+        return json.loads(file)
+    else:
+        return BLANK_SITE_LIST
+
+
+def save_site_list_obj(jsonObj):
+    file = open(get_data_folder() + SITES_LIST_FILE, 'w')
+    file.write(json.dumps(jsonObj))
+
+
+def get_data_folder():
+    sys = platform.system()
+    if 'Windows' in sys:
+        return get_data_folder_windows()
+    elif 'Linux' in sys:
+        return get_data_foler_linux()
+    elif 'Darwin' in sys:
+        return get_data_folder_macos()
+    else:
+        raise OSError("Unknown OS: " + sys)
+
+
+def get_data_folder_windows():
+    """ Returns data folder on windows. For example: C://Users/Nikita/AppData/Roaming/Koni Dev Team/Site Monster/
+
+    """
+    return os.getenv('APPDATA') + '/' + PATH
+
+
+def get_data_folder_macos():
+    """ Returns data folder on macOS. For example: /User/nikita/Library/Preferences/Koni Dev Team/Site Monster/'
+
+    """
+    return os.getenv('HOME') + '/Library/Preferences/' + PATH
+
+
+def get_data_foler_linux():
+    """ Returns data folder on Linux. For example: /home/nikita/.Koni Dev Team/Site Monster/'
+
+    """
+    return os.getenv('HOME') + '/.' + PATH
