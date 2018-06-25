e8844a8de30c6f6793fbd4541d0749ce70eaebb4
Grusha5480 <MrGraphitem@gmail.com>
2018-06-25 03:17:30 +0300

    site.py basics

--- site.py
+++ site.py
@@ -1,31 +1,44 @@
 import files
 
 
+def get_site_list():
+    return files.read('siteList.json')
+
+
+def save_site_list_obj(sites_obj):
+    files.save(sites_obj, 'siteList.json')
+
+
 def add(name, url, settings):
-    sites = files.get_site_list()
+    sites = get_site_list()
     if name in sites.keys():
         return "site_name_exists_error"
-    sites[name] = {"url": url, 'settings': settings}
-    files.save_site_list_obj(sites)
+    sites[name] = {"url": url, "settings": settings}
+    save_site_list_obj(sites)
 
 
 def delete(name):
-    sites = files.get_site_list()
+    sites = get_site_list()
     if name not in sites.keys():
         return "site_name_not_exists_error"
     del sites[name]
-    files.save_site_list_obj(sites)
+    save_site_list_obj(sites)
 
 
 def rename(old_name, new_name):
-    sites = files.get_site_list()
+    sites = get_site_list()
     if old_name not in sites.keys():
         return "site_name_not_exists_error"
     if new_name in sites.keys():
         return "site_name_exists_error"
     sites[new_name] = sites.pop(old_name)
-    files.save_site_list_obj(sites)
+    save_site_list_obj(sites)
 
 
 def get_list():
-    return files.get_site_list().keys()
+    return get_site_list().keys()
+
+
+def build_settings(method, headers, body, proxy, expected_code, expected_answer, sitemap_seatch):
+    return {"method": method, "headers": headers, "body": body, "proxy": proxy, "expected_code": expected_code,
+            "expected_answer": expected_answer, "sitemap_seatch": sitemap_seatch}
