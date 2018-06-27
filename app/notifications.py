71cdd5275ead65bd2e836b83923100cbfd2a727a
Nikita Serba <nikitaserba@icloud.com>
2018-06-28 01:04:04 +0300

    FUCK EM ALL

--- app/notifications.py
+++ app/notifications.py
@@ -1,36 +1,6 @@
 import appinfo
-import os
-
-from api import osinfo
+import plyer
 
 
-def send_windows_notification(message):
-    if not osinfo.is_win10():
-        raise OSError('Windows toast notifications can be sent only from Windows 10!')
-
-    import win10toast
-
-    toaster = win10toast.ToastNotifier()
-    toaster.show_toast(appinfo.APP_NAME,
-                       message,
-                       icon_path=appinfo.APP_ICON,
-                       duration=5,
-                       threaded=True)
-
-
-def send_linux_notification(message):
-    if not osinfo.is_linux():
-        raise OSError('Linux libnotify notifications can be sent only from linux-based OS!')
-    if appinfo.APP_ICON is not None:
-        os.system('notify-send "{}" "{}" -i {}'.format(appinfo.APP_NAME, message, os.path.abspath(appinfo.APP_ICON)))
-    else:
-        os.system('notify-send "{}" "{}"'.format(appinfo.APP_NAME, message))
-
-
-def send_mac_os_notification(message):
-    if not osinfo.is_mac_os():
-        raise OSError('MacOS notifications can be sent only from MacOS!')
-    if appinfo.APP_ICON is not None:
-        os.system('terminal-notifier -title "{}" -subtitle "" -message "{}" -appIcon "{}"'.format(appinfo.APP_NAME, message, appinfo.APP_ICON))
-    else:
-        os.system('terminal-notifier -title "{}" -subtitle "" -message "{}"'.format(appinfo.APP_NAME, message))
+def send_notification(message, title):
+    plyer.notification.notify(message=message, app_name=appinfo.APP_NAME, app_icon=appinfo.APP_ICON, title=title)
