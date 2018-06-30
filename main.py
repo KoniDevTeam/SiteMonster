1311a830d7662ecd6910e7ea2bac68f481121828
Grusha5480 <MrGraphitem@gmail.com>
2018-07-01 02:32:59 +0300

    typos

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.animation import Animation
 from kivy.animation import AnimationTransition
-# from app.notifications import *
+from app.notifications import *
 
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '1280')
@@ -17,7 +17,7 @@ Config.set('graphics', 'height', '720')
 class SiteMonster(App):
     BG_COLOR = (1, 1, 1, 1)
     TEXT_COLOR = (0, 0, 0, 1)
-    FONT_PATH = 'SegoeUI'
+    FONT_PATH = 'media/SegoeUI'
     WELCOME_H_TEXT = 'Welcome to SiteMonster!'
     WELCOME_T_TEXT = 'This app will notify you about any your server state.\n' \
                      'And it can make loud alarm if something went wrong :)'
@@ -36,17 +36,19 @@ class SiteMonster(App):
         welcome_text = Label(text=self.WELCOME_TEXT, font_name=self.FONT_PATH, color=self.TEXT_COLOR,
                              font_size='20pt', size_hint=(.15, .15))
         start_btn = Button(text=self.START_BTN_TEXT, font_name=self.FONT_PATH, size_hint=(.5, .2), padding_x=(20, 20))
+        quit_btn = Button(text="Quit", font_name=self.FONT_PATH, size_hint=(.5, .1), padding_x=(20, 20))
+        quit_btn.bind(on_press=quit)
 
         root.add_widget(Widget(size_hint=(1, .1)))
         root.add_widget(welcome_header)
         root.add_widget(welcome_title)
         root.add_widget(welcome_text)
         root.add_widget(start_btn)
-        root.add_widget(Widget(size_hint=(1, .1)))
+        root.add_widget(quit_btn)
 
         return root
 
 
 if __name__ == '__main__':
-    # send_notification("Ready to work!", "SiteMonster")
+#    send_notification("Ready to work!", "SiteMonster")
     SiteMonster().run()
