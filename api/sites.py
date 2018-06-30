efd0918f0a1743bb1e081e2e5a74519b2f9f9dbd
Nikita Serba <nikitaserba@icloud.com>
2018-07-01 01:27:47 +0300

    Bug fix

--- api/sites.py
+++ api/sites.py
@@ -13,7 +13,7 @@ def check(site) -> bool:
 
     try:
         r = requests.request(settings['method'], site['url'], headers=settings['headers'], data=settings['body'], proxies=settings['proxy'])
-    except ConnectionError:
+    except Exception:
         success = False
 
     return success and ((settings['expected_code'] is None and (200 <= r.status_code < 300)) or (r.status_code in settings['expected_code'])) and (settings['expected_answer'] is None or (r.text == settings['expected_answer']))
