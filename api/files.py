65819bd932ce80c932864c91a05d9bf523f01da0
Nikita Serba <nikitaserba@icloud.com>
2018-07-01 03:54:04 +0300

    Bug fix

--- api/files.py
+++ api/files.py
@@ -8,38 +8,6 @@ from api.osinfo import *
 PATH = 'Koni Dev Team/Site Monster/'
 
 
-def save(obj: object, filename: str):
-    """Make a json from obj and save to filename in data folder."""
-
-    f = open(get_data_folder() + filename, "w")
-    f.write(json.dumps(obj))
-
-
-def read(filename: str):
-    """Gets json from filename and loads it to dictionary/list."""
-
-    f = open(get_data_folder() + filename, "r")
-    return json.loads(f.read())
-
-
-def get_data_folder() -> str:
-    """Gets data folder.
-
-    For more information see docs for  functions `get_data_folder_windows`,
-    `get_data_folder_macos` and `get_data_folder_linux`.
-
-    """
-
-    if is_win():
-        return get_data_folder_windows()
-    elif is_linux():
-        return get_data_folder_linux()
-    elif is_mac_os():
-        return get_data_folder_macos()
-    else:
-        raise OSError("Unknown OS: " + platform.system())
-
-
 def get_data_folder_windows() -> str:
     """ Returns data folder on windows.
 
@@ -65,3 +33,35 @@ def get_data_folder_linux() -> str:
 
     """
     return os.getenv('HOME') + '/.' + PATH
+
+
+def get_data_folder() -> str:
+    """Gets data folder.
+
+    For more information see docs for  functions `get_data_folder_windows`,
+    `get_data_folder_macos` and `get_data_folder_linux`.
+
+    """
+
+    if is_win():
+        return get_data_folder_windows()
+    elif is_linux():
+        return get_data_folder_linux()
+    elif is_mac_os():
+        return get_data_folder_macos()
+    else:
+        raise OSError("Unknown OS: " + platform.system())
+
+
+def save(obj: object, filename: str):
+    """Make a json from obj and save to filename in data folder."""
+
+    f = open(get_data_folder() + filename, "w")
+    f.write(json.dumps(obj))
+
+
+def read(filename: str):
+    """Gets json from filename and loads it to dictionary/list."""
+
+    f = open(get_data_folder() + filename, "r")
+    return json.loads(f.read())
