f334168bc3c64a6813d9291e10a0055c9c74243c
Nikita Serba <nikitaserba@icloud.com>
2018-08-06 19:46:46 +0300

    Daemon code clean

--- main/daemon_main.py
+++ main/daemon_main.py
@@ -6,17 +6,24 @@ import app.site as site
 import app.notifications as notify
 from app import logger
 
+
+def check_site(name: str, site_data: dict):
+    if not sites.check(site_data):
+        fail_actions = site_data['settings']['fail_actions']
+        if fail_actions['send_notification']:
+            logging.warning('Site %s is down!!!' % name)
+            notify.send_notification('Site %s is down!!!' % name)
+        if fail_actions['play_sound']:
+            notify.play_sound()
+        time.sleep(30)
+
+
 if __name__ == '__main__':
     logger.init_log('daemon')
     logger.log_pc_info()
     while True:
         sites_dict = site.get_sites_dict()
         for i, j in sites_dict.items():
-            if not sites.check(j):
-                if j['settings']['fail_actions']['send_notification']:
-                    logging.warning('Site %s is down!!!' % i)
-                    notify.send_notification('Site %s is down!!!' % i)
-                if j['settings']['fail_actions']['play_sound']:
-                    notify.play_sound()
-                time.sleep(30)
+            logging.info("Checking " + i)
+            check_site(i, j)
         time.sleep(1)
