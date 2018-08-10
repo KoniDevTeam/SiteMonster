f4de69637ff2edd8ea944b1950e35491a8778ce2
Grusha5480 <MrGraphitem@gmail.com>
2018-08-10 22:39:16 +0300

    license

--- gui/ui/SiteMonster.py
+++ gui/ui/SiteMonster.py
@@ -1,4 +1,22 @@
-from PyQt5 import QtWidgets, QtCore
+# Copyright (C) 2018 Koni Dev Team, All Rights Reserved
+# https://github.com/KoniDevTeam/SiteMonster/
+#
+# This file is part of Site Monster.
+#
+# Site Monster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Site Monster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Site Monster.  If not, see <https://www.gnu.org/licenses/>.
+
+from PyQt5 import QtWidgets
 from gui.ui import welcome as design
 from gui.ui.SiteMonitor import SiteMonitor
 from api.gui import *
